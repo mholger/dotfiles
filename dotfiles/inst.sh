253f645726bb4d3c2b4bfbe2071fe0e070d391af
M. Holger <matt@paralleltheory.com>
2014-12-18 20:33:18 -0600

    Fixing install script

--- dotfiles/inst.sh
+++ dotfiles/inst.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mkdir ~/dotfiles
-cd ~/dotfiles
-
+pushd .
+cd ~
 git clone --no-checkout https://github.com/mholger/dotfiles.git
+
+cd dotfiles
 git config core.worktree "../../"
 
 git reset --hard origin/master
 
+popd
